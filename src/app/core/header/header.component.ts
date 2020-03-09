6b42ddbd2e1bf39bda0d6d8bc865b541677971d4
Abdallah <abdallahragab40@gmail.com>
2020-03-10 01:49:37 +0200

    add search page

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { UserService } from "./../../features/user/user.service";
 import { User } from "./../../../_model/user";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-header",
@@ -9,10 +10,17 @@ import { User } from "./../../../_model/user";
 })
 export class HeaderComponent implements OnInit {
   user: User;
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit() {
     // To be Changed (Auth)
     this.user = this.userService.getById(0);
+  }
+
+  submitSearchVal(event) {
+    if (event.key === "Enter") {
+      this.router.navigate(["/search/", event.target.value]);
+    }
+    return;
   }
 }
