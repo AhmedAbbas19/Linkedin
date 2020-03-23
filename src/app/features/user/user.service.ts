fe20657a05c30d7950d36ea6eca053fb7961934e
Amir <59926940+Amir-Mohammed@users.noreply.github.com>
2020-03-23 17:29:43 +0200

    update4

--- src/app/features/user/user.service.ts
+++ src/app/features/user/user.service.ts
@@ -621,4 +621,35 @@ export class UserService {
     const index = this.users.findIndex(u => u.id === id);
     this.users.splice(index, 1);
   }
+  // getAll() {
+  //   return this.http.get(this.url).pipe(catchError(this.handleError));
+  // }
+
+  // getById(id: number) {
+  //   return this.http.get(this.url + "/" + id);
+  // }
+
+  // addUser(user) {
+  //   return this.http.post(this.url, user);
+  // }
+
+  // updateUser(user) {
+  //   return this.http.put(this.url + "/" + user.id, user);
+  // }
+
+  // deleteUser(id) {
+  //   return this.http
+  //     .delete(this.url + "/" + id)
+  //     .pipe(catchError(this.handleError));
+  // }
+
+  // handleError(error: Response) {
+  //   if (error.status === 404) {
+  //     return throwError(new NotFoundError());
+  //   } else if (error.status === 400) {
+  //     return throwError(new BadRequestError());
+  //   } else {
+  //     return throwError(new AppError(error));
+  //   }
+  // }
 }
